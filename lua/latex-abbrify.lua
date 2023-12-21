40df404a64f05ac4134b17c2b76b09a3cdf0950e
David Schatz <david.schatz@tu-ilmenau.de>
2023-12-21 11:01:59 +0100

    Scan the complete working directory only once

--- lua/latex-abbrify.lua
+++ lua/latex-abbrify.lua
@@ -1,50 +1,54 @@
-local M = {}
+local M = {
+    scan_count = 0,
+    acronyms = {}
+}
 
 local function get_tex_files()
-    return vim.fn.glob(vim.fn.getcwd() .. '/**/*.tex', false, true)
+    return vim.fn.glob(vim.fn.getcwd() .. '/**/*.tex', true, true)
 end
 
 local function extract_acronym_definitions(file_path)
-    local acronyms = {}
-
     for line in io.lines(file_path) do
         -- TODO other forms, e.g., \acrodef{...}
         -- TODO "-" not supported in abbreviations?
         for acronym in line:gmatch("\\DeclareAcronym{([^{}%-]*)}") do
-            table.insert(acronyms, acronym)
+            M.acronyms[acronym] = true
         end
     end
-
-    return acronyms
 end
 
-local function main()
-    -- TODO some clever caching would be nice
-    local tex_files = get_tex_files()
+local function set_abbreviations()
+    for acronym, _ in pairs(M.acronyms) do
+        local iabbrev_cmd = "iabbrev"
+        local acronym_plural = acronym .. "s"
+        local ac = "\\ac{" .. acronym .. "}"
+        local acp = "\\acp{" .. acronym .. "}"
 
-    for _, file_path in ipairs(tex_files) do
-        for _, acronym in ipairs(extract_acronym_definitions(file_path)) do
-            local iabbrev_cmd = "iabbrev <buffer>"
-            local acronym_plural = acronym .. "s"
-            local ac = "\\ac{" .. acronym .. "}"
-            local acp = "\\acp{" .. acronym .. "}"
-
-            vim.cmd(iabbrev_cmd .. " " .. acronym .. " " .. ac)
-            vim.cmd(iabbrev_cmd .. " " .. acronym_plural .. " " .. acp)
-        end
+        vim.cmd(iabbrev_cmd .. " " .. acronym .. " " .. ac)
+        vim.cmd(iabbrev_cmd .. " " .. acronym_plural .. " " .. acp)
     end
+end
+
+local function initialize()
+    M.scan_count = M.scan_count + 1
+    local tex_files = get_tex_files()
+    for _, file_path in ipairs(tex_files) do
+        extract_acronym_definitions(file_path)
+    end
+    set_abbreviations()
 end
 
 function M.setup()
     local augroup = vim.api.nvim_create_augroup("LaTeXAbbrify", { clear = true })
     -- TODO also on save
     vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWinEnter" },
+        { "BufEnter" },
         {
             group = augroup,
             desc = "LaTeX abbrify!",
             pattern = { "*.tex" },
-            callback = main,
+            callback = initialize,
+            once = true,
         }
     )
 end

74c75b59ce1677b52073f777fe8bb68fc38dfac8
David Schatz <david.schatz@tu-ilmenau.de>
2025-11-25 15:15:53 +0100

    Format (indent 3 spaces)

--- lua/latex-abbrify.lua
+++ lua/latex-abbrify.lua
@@ -1,75 +1,74 @@
 local M = {
-    scan_count = 0,
-    acronyms = {}
+   scan_count = 0,
+   acronyms = {}
 }
 
 local function get_tex_files()
-    return vim.fn.glob(vim.fn.getcwd() .. '/**/*.tex', true, true)
+   return vim.fn.glob(vim.fn.getcwd() .. '/**/*.tex', true, true)
 end
 
 local function extract_acronym_definitions(file_path)
-    for line in io.lines(file_path) do
-        -- TODO other forms, e.g., \acrodef{...}
-        -- TODO "-" not supported in abbreviations?
-        for acronym in line:gmatch("\\DeclareAcronym{([^{}%-]*)}") do
-            M.acronyms[acronym] = true
-        end
-    end
+   for line in io.lines(file_path) do
+      -- TODO "-" not supported in abbreviations?
+      for acronym in line:gmatch("\\DeclareAcronym{([^{}%-]*)}") do
+         M.acronyms[acronym] = true
+      end
+   end
 end
 
 local function set_abbreviations()
-    for acronym, _ in pairs(M.acronyms) do
-        local iabbrev_cmd = "iabbrev"
-        local acronym_plural = acronym .. "s"
-        local ac = "\\ac{" .. acronym .. "}"
-        local acp = "\\acp{" .. acronym .. "}"
+   for acronym, _ in pairs(M.acronyms) do
+      local iabbrev_cmd = "iabbrev"
+      local acronym_plural = acronym .. "s"
+      local ac = "\\ac{" .. acronym .. "}"
+      local acp = "\\acp{" .. acronym .. "}"
 
-        vim.cmd(iabbrev_cmd .. " " .. acronym .. " " .. ac)
-        vim.cmd(iabbrev_cmd .. " " .. acronym_plural .. " " .. acp)
-    end
+      vim.cmd(iabbrev_cmd .. " " .. acronym .. " " .. ac)
+      vim.cmd(iabbrev_cmd .. " " .. acronym_plural .. " " .. acp)
+   end
 end
 
 local function handle_write()
-    extract_acronym_definitions(vim.fn.expand("%"))
-    set_abbreviations()
+   extract_acronym_definitions(vim.fn.expand("%"))
+   set_abbreviations()
 end
 
 local function initialize()
-    M.scan_count = M.scan_count + 1
-    local tex_files = get_tex_files()
-    for _, file_path in ipairs(tex_files) do
-        extract_acronym_definitions(file_path)
-    end
-    set_abbreviations()
+   M.scan_count = M.scan_count + 1
+   local tex_files = get_tex_files()
+   for _, file_path in ipairs(tex_files) do
+      extract_acronym_definitions(file_path)
+   end
+   set_abbreviations()
 end
 
 function M.setup()
-    local augroup = vim.api.nvim_create_augroup("LaTeXAbbrify", { clear = true })
+   local augroup = vim.api.nvim_create_augroup("LaTeXAbbrify", { clear = true })
 
-    vim.api.nvim_create_autocmd(
-        { "BufEnter" },
-        {
-            group = augroup,
-            desc = "LaTeX abbrify init",
-            pattern = { "*.tex" },
-            callback = initialize,
-            once = true,
-        }
-    )
+   vim.api.nvim_create_autocmd(
+      { "BufEnter" },
+      {
+         group = augroup,
+         desc = "LaTeX abbrify init",
+         pattern = { "*.tex" },
+         callback = initialize,
+         once = true,
+      }
+   )
 
-    vim.api.nvim_create_autocmd(
-        { "BufWritePost" },
-        {
-            group = augroup,
-            desc = "LaTeX abbrify update",
-            pattern = { "*.tex" },
-            callback = handle_write,
-        }
-    )
+   vim.api.nvim_create_autocmd(
+      { "BufWritePost" },
+      {
+         group = augroup,
+         desc = "LaTeX abbrify update",
+         pattern = { "*.tex" },
+         callback = handle_write,
+      }
+   )
 end
 
 function M.acronym_iter()
-    return next, M.acronyms, nil
+   return next, M.acronyms, nil
 end
 
 return M
